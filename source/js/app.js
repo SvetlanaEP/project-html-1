ed3f877e674a4bcc9e31ab10ee004f4337caff42
Svetlana <se55petrova@gmail.com>
2024-12-29 17:58:37 +0500

    заполнила контентом разделы, сделала модальное окно с формой

--- source/js/app.js
+++ source/js/app.js
@@ -1,4 +1,7 @@
+const bodyTeg = document.querySelector('body')
+
 let navMain = document.querySelector('.main-nav');
+
 let navToggle = document.querySelector('.main-nav__toggle');
 
 navMain.classList.remove('main-nav--nojs');
@@ -12,3 +15,26 @@ navToggle.addEventListener('click', function () {
     navMain.classList.remove('main-nav--opened');
   }
 });
+
+let orderButtonList = document.querySelectorAll('.button--order');
+let modalCallback = document.querySelector('.form-modal');
+
+console.log(orderButtonList)
+
+orderButtonList.forEach((buttonSend) => {
+ buttonSend.addEventListener('click', () => {
+    bodyTeg.style.overflow = 'hidden';
+    modalCallback.style.display = 'block';
+    document.querySelector('.overflow').classList.add('overflow--open');
+  })
+})
+
+let closeButton = modalCallback.querySelector('.form-modal__closed')
+
+closeButton.addEventListener('click', () => {
+  bodyTeg.style.overflow = 'auto';
+  modalCallback.style.display = 'none';
+  document.querySelector('.overflow').classList.remove('overflow--open');
+})
+
+
